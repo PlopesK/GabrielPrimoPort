9648678c833bfdb2d9fbded564d89ef18a393e8c
PlopesK <gp081009@gmail.com>
2025-05-18 22:14:27 -0300

    Página inicial completa

--- src/script.js
+++ src/script.js
@@ -10,7 +10,66 @@ const icons = [
 const baseURL = "https://raw.githubusercontent.com/portfolio-projetos-dev/assets/main/images/";
 const list = document.getElementById("iconList");
 
+const projetos = [
+  {
+    imagem: "./imgs/sonicr.png",
+    titulo: "Sonic R",
+    descricao: "Um jogo de Sonic para navegador feito com HTML, CSS e JS",
+    link: "https://plopesk.github.io/Sonic-R/"
+  },
+  {
+    imagem: "./imgs/pokedex.png",
+    titulo: "PokeDex",
+    descricao: "Um site que apresenta detalhes de Pokémons da Primeira Geração.",
+    link: "https://plopesk.github.io/Pokedex/"
+  },
+  {
+    imagem: "./imgs/anemo.png",
+    titulo: "Anemo System",
+    descricao: "Sistema de controle escolar feito para o TCC da ETEC.",
+    link: "https://anemosystem.github.io/anemo-system-website/"
+  }
+];
+
+// Carrega de maneira dinâmica os projetos dentro do array "projetos" //
+function carregarProjetos() {
+  const ul = document.getElementById("projeList");
+
+  projetos.forEach((projeto) => {
+    const li = document.createElement("li");
+
+    const div = document.createElement("div");
+    div.className = "projeConteudo";
+    //Para cara um dos items do array, cria um <li><div class="projeConteudo"></div></li>
+
+    const img = new Image();
+    img.src = projeto.imagem;
+    img.alt = projeto.titulo;
+    img.loading = "lazy";
+
+    const h3 = document.createElement("h3");
+    h3.textContent = projeto.titulo;
+
+    const p = document.createElement("p");
+    p.textContent = projeto.descricao;
+
+    const a = document.createElement("a");
+    a.href = projeto.link;
+    a.textContent = "Ver repositório";
+    a.target = "_blank";
+    a.rel = "noopener noreferrer";
+
+    div.appendChild(img);
+    div.appendChild(h3);
+    div.appendChild(p);
+    div.appendChild(a);
+    li.appendChild(div);
+    ul.appendChild(li);
+  });
+}
+
 window.addEventListener("DOMContentLoaded", function () {
+    //Quando a página carregar, carrega os ícones e os projetos
     icons.forEach(icon => {
         const li = document.createElement("li");
         const img = document.createElement("img");
@@ -21,4 +80,5 @@ window.addEventListener("DOMContentLoaded", function () {
         li.appendChild(img);
         list.appendChild(li);
     });
+    carregarProjetos()
 })
